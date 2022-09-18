1d3c5ca2d519d50b44313dba85c929d9f6505d04
ben-bergan <68228476+ben-bergan@users.noreply.github.com>
2022-09-18 16:02:04 +1000

    Update streamlit_strava_app.py

--- Scripts/streamlit_strava_app.py
+++ Scripts/streamlit_strava_app.py
@@ -19,7 +19,6 @@ df['race_counter'] = df.run_category.apply(race_count)
 analysis = df[df['year']==2022]
 distance = analysis.groupby('month').agg({'distance_km':'sum'}).reset_index()
 elevation = analysis.groupby('month').agg({'total_elevation_gain':'sum'}).reset_index()
-kudo = analysis.groupby('month').agg({'kudos_count':'sum'}).reset_index()
 summary = df.groupby('year').agg({'distance_km':'sum','total_elevation_gain':'sum','race_counter':'sum'}).reset_index()
 
 st.write("""# Strava Data Analysis
@@ -30,8 +29,6 @@ st.caption ('This chart displays the km''s ran per month in 2022')
 st.bar_chart(distance, x='month', y='distance_km')
 st.caption ('This chart displays total elevation gain per month in 2022')
 st.line_chart(elevation, x='month',y='total_elevation_gain')
-st.caption ('This chart displays strava wanker count per month in 2022')
-st.line_chart(kudo, x='month',y='kudos_count')
 st.caption ('The table below compares the year on year performance')
 st.table(summary)
 
