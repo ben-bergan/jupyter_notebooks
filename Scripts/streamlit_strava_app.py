24d49aa020c34fa8404fbe4d97315cd89f790b79
ben-bergan <68228476+ben-bergan@users.noreply.github.com>
2022-10-01 14:09:43 +1000

    Update streamlit_strava_app.py

--- Scripts/streamlit_strava_app.py
+++ Scripts/streamlit_strava_app.py
@@ -2,12 +2,13 @@ from sre_constants import ANY_ALL
 import pandas as pd
 import streamlit as st
 from pathlib import Path
+import numpy as np
 
 strava_run_data = Path(__file__).parents[0] / 'strava_run_data.csv'
 
 df = pd.read_csv(strava_run_data)
 
-df.drop(columns={'start_date','workout_type','distance','moving_time','elapsed_time','average_speed'},inplace=True)
+df.drop(columns={'achievement_count','comment_count','kudos_count','week'},inplace=True)
 
 def race_count(run_category):
     if run_category == 'Race':
@@ -17,23 +18,40 @@ def race_count(run_category):
 
 df['race_counter'] = df.run_category.apply(race_count)      
 
-st.write("""# Yearly Strava Data Analysis
-  Created by *Ben Bergan*""")
+st.write("""# Strava Data Analysis 
+  Version 1.1
+  Created by Ben Bergan""")
 
 year_select = df.year.unique().tolist()
 
 option = st.selectbox('Select the year:',year_select)
+delta = option-1
 
 analysis = df[df['year']==option]
+analysis_prior = df[df['year']==delta]
 
 distance = analysis.groupby('month').agg({'distance_km':'sum'}).reset_index()
+distance_year = analysis.groupby('year').agg({'distance_km':'sum'}).reset_index(drop=True)
+distance_year = np.array(distance_year)[0]
+distance_prior_year = analysis_prior.groupby('year').agg({'distance_km':'sum'}).reset_index(drop=True)
+distance_prior_year = np.array(distance_prior_year)[0]
+
 elevation = analysis.groupby('month').agg({'total_elevation_gain':'sum'}).reset_index()
+elevation_year = analysis.groupby('year').agg({'total_elevation_gain':'sum'}).reset_index(drop=True)
+elevation_year = np.array(elevation_year)[0]
+elevation_prior_year = analysis_prior.groupby('year').agg({'total_elevation_gain':'sum'}).reset_index(drop=True)
+elevation_prior_year = np.array(elevation_prior_year)[0]
+
 summary = df.groupby('year').agg({'distance_km':'sum','total_elevation_gain':'sum','race_counter':'sum'}).reset_index()
 summary = summary.sort_values(by='year', ascending=False)
 summary.rename(columns={'distance_km':'Distance','total_elevation_gain':'Elevation Gain','race_counter':'Races'},inplace=True)
 year_summary = df[(df['year']==option) & (df['race_counter']==1)]
-year_summary = year_summary[['name','start_date_converted','distance_km','moving_time_converted','pr_count']]
-year_summary.rename(columns={'name':'Race Name','start_date_converted':'Race Date','distance_km':'Distance','moving_time_converted':'Moving Time','pr_count':'PB Count'},inplace=True)
+year_summary = year_summary[['name','start_date_converted','distance_km','run_elapsed_time','average_cadence','average_heartrate']]
+year_summary.rename(columns={'name':'Race Name','start_date_converted':'Race Date','distance_km':'Distance','run_elapsed_time':'Race Time','average_heartrate':'Average Heartrate'},inplace=True)
+
+col1, col2 = st.columns(2)
+col1.metric('Yearly Distance - Compared to Prior Year', distance_year, round(float(distance_year - distance_prior_year),2))
+col2.metric('Yearly Elevation Gain - Compared to Prior Year', elevation_year, round(float(elevation_year - elevation_prior_year),2))
 
 st.caption ('This chart displays the km''s ran per month in the selected year')
 st.bar_chart(distance, x='month', y='distance_km')
@@ -43,4 +61,3 @@ st.caption ('The table below lists all races in the selected year')
 st.table(year_summary)
 st.caption ('The table below compares the year on year performance')
 st.table(summary)
-
